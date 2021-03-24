d46edad1a31dfc2078c949ed7a1195766ccf2878
Christopher Schmidt <christopher.schmidt@posteo.de>
2021-03-24 15:21:33 +0100

    :zap: Remove popovers, as they seem to have negative impact on responsiveness

--- src/components/ImageSelection.vue
+++ src/components/ImageSelection.vue
@@ -25,7 +25,7 @@
         </tr>
         </thead>
         <tbody>
-        <tr v-for="(image, path) in sortedImages" :key="path" v-b-popover.hover.top="image.path">
+        <tr v-for="(image, path) in sortedImages" :key="path">
           <td>{{ image.filename}}</td>
           <td>{{ image.iso }}</td>
           <td>{{ image.width}}</td>
