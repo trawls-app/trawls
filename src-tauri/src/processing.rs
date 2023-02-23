3c18a8f6c2fe3818c14999d8e07b2f33710637fc
Christopher Schmidt <christopher.schmidt@posteo.de>
2023-02-23 20:48:32 +0100

    🧑‍💻♻️  Simplify loading/merging with map-reduce-approach

--- src-tauri/src/processing.rs
+++ src-tauri/src/processing.rs
@@ -1,20 +1,15 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
-use std::thread::JoinHandle;
-use std::{thread, time};
+use std::time;
 
 use anyhow::{self, Context};
 use clap::ValueEnum;
-use itertools::chain;
-use log::{error, info};
+use log::info;
 use rawler::exif::Exif;
 use rayon::prelude::*;
 use serde::Serialize;
 
-use crate::processing::image::MergeMode::{Maximize, WeightedAverage};
-use crate::processing::image::{Image, Mergable, MergeMode};
-
-use self::status::Status;
+use crate::processing::image::{Frame, Image};
 
 pub mod cli_progress;
 mod dng_writing;
@@ -82,8 +77,6 @@ pub fn run_merge(
         num_threads
     );
 
-    let queue_lights = Arc::new(Mutex::new(vec![]));
-    let queue_darks = Arc::new(Mutex::new(vec![]));
     let mut tasks: Vec<LoadTask> = lightframe_files
         .iter()
         .enumerate()
@@ -102,38 +95,17 @@ pub fn run_merge(
             .collect(),
     );
 
-    // Start workers
-    let thread_handles_lights = spawn_workers(num_threads, Arc::clone(&queue_lights), Maximize, state.clone());
-    let thread_handles_darks = spawn_workers(num_threads, Arc::clone(&queue_darks), WeightedAverage, state.clone());
-
-    // Load light- and darkframes
-    tasks
+    let frame = tasks
         .par_iter()
-        .map(|t| {
-            if state.lock().unwrap().aborted() {
-                return Ok(());
-            }
-            if let Err(e) = load_image(t, Arc::clone(&queue_lights), Arc::clone(&queue_darks), mode, state.clone()) {
-                state.lock().unwrap().abort();
-                return Err(e);
-            }
+        .map(|t| load_image(t, mode, state.clone()))
+        .reduce(|| Ok(Box::new(Frame::identity())), |x, y| x?.merge(*y?, state.clone()));
 
-            Ok(())
-        })
-        .collect::<anyhow::Result<()>>()?;
-
-    for t in chain(thread_handles_lights, thread_handles_darks) {
-        t.join().unwrap()?;
-    }
-
-    let lightframe = queue_lights.lock().unwrap().pop().unwrap();
-    let raw_image = if darkframe_files.is_empty() {
-        info!("No darkframes selected");
-        lightframe
-    } else {
-        info!("Subtracting averaged darkframe");
-        let darkframe = queue_darks.lock().unwrap().pop().unwrap();
-        lightframe.apply_darkframe(darkframe)?
+    let raw_image = match frame {
+        Ok(x) => x.get_image()?,
+        Err(e) => {
+            state.lock().unwrap().abort();
+            return Err(e);
+        }
     };
 
     info!("Processing done");
@@ -155,106 +127,30 @@ pub fn run_merge(
     Ok(RenderedPreview::new(preview_bytes, exif))
 }
 
-fn spawn_workers(
-    num_threads: usize,
-    queue: Arc<Mutex<Vec<Image>>>,
-    merge_mode: MergeMode,
-    state: Arc<Mutex<status::ProcessingStatus>>,
-) -> Vec<JoinHandle<anyhow::Result<()>>> {
-    let mut thread_handles = vec![];
-
-    for _ in 0..num_threads {
-        let q = Arc::clone(&queue);
-        let s = state.clone();
-        thread_handles.push(thread::spawn(move || -> anyhow::Result<()> { queue_worker(q, merge_mode, s) }));
-    }
-
-    thread_handles
-}
-
-fn queue_worker(
-    queue: Arc<Mutex<Vec<Image>>>,
-    merge_mode: MergeMode,
-    state: Arc<Mutex<status::ProcessingStatus>>,
-) -> anyhow::Result<()> {
-    loop {
-        // Stop if any other thread exited preliminary
-        if state.lock().unwrap().aborted() {
-            return Ok(());
-        }
-
-        let mut q = queue.lock().unwrap();
-        if q.len() <= 1 {
-            if state.lock().unwrap().loading_done() {
-                return Ok(());
-            } else {
-                // Queue is empty but work is not done yet => Wait.
-                drop(q);
-                thread::sleep(time::Duration::from_millis(20));
-                continue;
-            }
-        }
-        state.lock().unwrap().start_merging();
-
-        let v1 = q.pop().unwrap();
-        let v2 = q.pop().unwrap();
-        drop(q);
-
-        let res = match v1.merge(v2, merge_mode).with_context(|| "Failed to merge images.") {
-            Ok(x) => x,
-            Err(err) => {
-                error!("{:?}", err);
-                state.lock().unwrap().abort();
-                return Err(err);
-            }
-        };
-
-        queue.lock().unwrap().push(res);
-        state.lock().unwrap().finish_merging();
-    }
-}
-
 fn load_image(
     task: &LoadTask,
-    queue_lights: Arc<Mutex<Vec<Image>>>,
-    queue_darks: Arc<Mutex<Vec<Image>>>,
     comets: Comets,
     state: Arc<Mutex<status::ProcessingStatus>>,
-) -> anyhow::Result<()> {
+) -> anyhow::Result<Box<Frame>> {
     let count_lights = state.lock().unwrap().count_lights;
-    state.lock().unwrap().start_loading();
-
-    match task.frame_type {
-        FrameType::Lightframe(index) => load_lightframe(task.path.as_path(), queue_lights, index, count_lights, comets)
-            .with_context(|| format!("Could not load lightframe '{}'", task.path.display()))?,
-        FrameType::Darkframe => load_darkframe(task.path.as_path(), queue_darks)
-            .with_context(|| format!("Could not load darkframe '{}'", task.path.display()))?,
-    }
-
-    state.lock().unwrap().finish_loading();
-    Ok(())
-}
-
-fn load_lightframe(
-    entry: &Path,
-    queue: Arc<Mutex<Vec<Image>>>,
-    index: usize,
-    num_images: usize,
-    comets: Comets,
-) -> anyhow::Result<()> {
-    let intensity = match comets {
-        Comets::Falling => 1.0 - index as f32 / num_images as f32,
-        Comets::Raising => index as f32 / num_images as f32,
-        Comets::Normal => 1.0,
+    let intensity = match task.frame_type {
+        FrameType::Lightframe(index) => match comets {
+            Comets::Falling => 1.0 - index as f32 / count_lights as f32,
+            Comets::Raising => index as f32 / count_lights as f32,
+            Comets::Normal => 1.0,
+        },
+        FrameType::Darkframe => 1.0,
     };
 
-    let img = Image::from_raw_file(entry, intensity)?;
-    queue.lock().unwrap().push(img);
-    Ok(())
-}
+    state.lock().unwrap().start_loading();
+    let img = Image::from_raw_file(task.path.as_path(), intensity)
+        .with_context(|| format!("Could not load file {:#?}", task.path))?;
+    state.lock().unwrap().finish_loading();
 
-fn load_darkframe(entry: &Path, queue: Arc<Mutex<Vec<Image>>>) -> anyhow::Result<()> {
-    let img = Image::from_raw_file(entry, 1.0)?;
-    queue.lock().unwrap().push(img);
-    Ok(())
+    let frame = match task.frame_type {
+        FrameType::Lightframe(_) => Frame::from_lightframe(img),
+        FrameType::Darkframe => Frame::from_darkframe(img),
+    };
+
+    Ok(Box::new(frame))
 }
